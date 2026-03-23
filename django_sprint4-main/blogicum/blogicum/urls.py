267e668bf76a7cdcd96e6af40eec9895e6cad474
Flwrlst7 <91014488+TarelkaRubic@users.noreply.github.com>
2026-03-23 22:48:26 +0300

    ф

--- django_sprint4-main/blogicum/blogicum/urls.py
+++ django_sprint4-main/blogicum/blogicum/urls.py
@@ -9,18 +9,3 @@ from django.urls import include, path, reverse_lazy
 handler404 = 'pages.views.page_not_found'
 handler500 = 'pages.views.internal_server_error'
 
-urlpatterns = [
-    path('', include('blog.urls')),
-    path('pages/', include('pages.urls')),
-    path('admin/', admin.site.urls),
-    path('auth/', include('django.contrib.auth.urls')),
-    path(
-        'auth/registration/',
-        CreateView.as_view(
-            template_name='registration/registration_form.html',
-            form_class=UserCreationForm,
-            success_url=reverse_lazy('blog:index'),
-        ),
-        name='registration',
-    ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
