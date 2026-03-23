a8e3272a43cff6b4e14ea03a1b9449648a0e8e3b
Flwrlst7 <91014488+TarelkaRubic@users.noreply.github.com>
2026-03-23 22:49:42 +0300

    п

--- django_sprint4-main/blogicum/blog/models.py
+++ django_sprint4-main/blogicum/blog/models.py
@@ -35,5 +35,65 @@ class Category(BaseModel):
         verbose_name_plural = 'Категории'
 
 
+class Location(BaseModel):
+    name = models.CharField(max_length=TEXT_LENGTH,
+                            verbose_name='Название места')
+
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
+
+
+class Post(BaseModel):
+    title = models.CharField(max_length=TEXT_LENGTH, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+        help_text='Если установить дату и время в будущем — '
+                  'можно делать отложенные публикации.'
+    )
+    image = models.ImageField('Фото', upload_to='posts_images', blank=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор публикации',
+        related_name='posts'
+    )
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Местоположение',
+        related_name='posts'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Категория',
+        related_name='posts'
+    )
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+
+
+class Comment(models.Model):
+    text = models.TextField('Текст комментария')
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        verbose_name='Комментарий',
+        related_name='comments',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('created_at',)
+
     def __str__(self) -> str:
         return self.text
